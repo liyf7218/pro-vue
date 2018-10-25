ff773c26b79d05e9fd3e734ccb9057d44ac890e8
liyf7218 <liyf7218@163.com>
2018-10-25 17:33:26 +0800

    init local git-repo

---
+++ src/containers/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container class='container'>
+    <!-- header -->
+    <el-header>
+      <i-header />
+    </el-header>
+
+    <!-- main -->
+    <el-main>
+      <i-main />
+    </el-main>
+
+  </el-container>
+</template>
+
+<script>
+import iHeader from './header'
+import iMain from './main'
+export default {
+  name: 'container',
+  components: {
+    iHeader,
+    iMain
+  }
+}
+</script>
+
+<style scoped>
+.el-header {
+  border-bottom: 1px solid lightsteelblue;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+}
+</style>
---
+++ src/containers/main/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="main">
+    main
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'iMain',
+  data () {
+    return {
+      msg: 'this is the iMain.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
